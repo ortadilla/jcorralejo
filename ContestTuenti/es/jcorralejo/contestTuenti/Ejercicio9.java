ec051c2425e8dd303a83bbd7267200989c1ba734
ortadilla@gmail.com <ortadilla@gmail.com@12cb7d04-6001-11de-8abc-d1c337b90d21>
2011-06-16 12:56:53 +0000

    #GEOS8823 · Se añaden los criterios de búsqueda almacen origen, TipoDesti + Destino, GC y nº de pedido interno

--- ContestTuenti/es/jcorralejo/contestTuenti/Ejercicio9.java
+++ ContestTuenti/es/jcorralejo/contestTuenti/Ejercicio9.java
@@ -6,25 +6,20 @@ import java.util.StringTokenizer;
 
 public class Ejercicio9 {
 
-	private static int MAX_PRUEBAS = 100; 
-	private static int MAX_L = 64; 
-	private static int MAX_T = 1000000; 
+//	private static int MAX_PRUEBAS = 1; 
 	
-	private static boolean[][] tablero = new boolean[MAX_L][MAX_T];
-
 	public static void main (String[] args){
 		try{
 			
-			cargarTablero();
-
 			BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 			int i=0;
-			while(i<MAX_PRUEBAS){
+//			while(i<MAX_PRUEBAS){
+				
 				String linea = br.readLine();
 				if(linea!=null && !"".equals(linea)){
 					int N = 0;
 					N = Integer.parseInt(String.valueOf(linea.charAt(0)));
-					for(int j=0; j<N; j++){
+					for(int j=0; j<N-1; j++){
 						int l = 0;
 						int t = 0;
 						linea = br.readLine();
@@ -39,44 +34,31 @@ public class Ejercicio9 {
 							if(tokens.hasMoreTokens())
 								t = Integer.parseInt(tokens.nextToken());
 						}
-
+						
+						
+						
 						boolean ninguno = true;
-						System.out.println();
-						for(int k=0; k<l; k++){
-							if(tablero[t][k]){
-								System.out.print(k+" ");
+						int min = Math.min(t, l);
+						String cad = "";
+						for(int k=0; k<min;k++){
+							if(t%2==0 && k%2!=0 || t%2!=0 && k%2==0){
+								cad  += k+" ";
 								ninguno = false;
 							}
 						}
 						if(ninguno)
-							System.out.println("All lights are off :(");
-
+							cad = "All lights are off :(";
+						else
+							cad = cad.substring(0, cad.length()-1);
+						System.out.println(cad);
+						
 					}
 				}
-			}
+//			}
 
 			i++;
 		}catch (Exception e){
 			e.printStackTrace();
 		}
 	}
-	
-	private static void cargarTablero(){
-		for(int i=0; i<MAX_L; i++){
-			for(int j=0; j<MAX_T; j++){
-				if(i>j){
-					if((i%2==0 && j%2!=0) || (i%2!=0 && j%2==0))
-						tablero[i][j] = true;	
-				}
-			}
-		}
-		
-//		for(int i=0; i<MAX_L; i++){
-//			for(int j=0; j<MAX_T; j++){
-//				System.out.print(tablero[i][j] ? '1' : '0');
-//			}
-//			System.out.println();
-//		}
-	}
-
 }
